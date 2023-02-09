b2b44bfa281b72bee0d7715c4614c4980a2a0709
Illia Shatokhin <resonance.2106@gmail.com>
2023-02-09 13:26:04 +0200

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 200,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
