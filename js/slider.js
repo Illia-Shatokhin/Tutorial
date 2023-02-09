8f41f5844e724b3b023aff1c110320ddd42c6472
Illia Shatokhin <resonance.2106@gmail.com>
2023-02-09 14:50:11 +0200

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,26 @@
 var swiper = new Swiper(".mySwiper", {
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 4,
   spaceBetween: 200,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 50
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 100
+      },
+      // when window width is >= 640px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 200
+      }
     },
   });
